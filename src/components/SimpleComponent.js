cdf1a77e4cfdd65a1fb87f66bdb8b304741921d1
whosAle <abelgrave95@gmail.com>
2019-05-16 13:30:54 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponent Here
+import React from 'react';
+
+class ControlledInput extends React.Component {
+  state = {
+    mood: 'happy',
+  }
+
+  handleClick = event => {
+    this.setState({
+      mood: this.state.mood === "happy" ? "sad" : "happy" ,
+    });
+  }
+
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    );
+  }
+}
+
+export default ControlledInput;
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+const MenuBar = (props) => {
+
+  return (
+    <div onClick={props.handleClick}>I am just happy</div>
+  )
+}
+
+export default MenuBar
